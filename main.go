1128a12332423f0104113471bddf2889d2c1508c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-01-17 17:11:00 +0100

    Code cleanup

--- main.go
+++ main.go
@@ -21,61 +21,8 @@ import (
 const (
 	metricsEndpoint      = "/metrics"
 	notificationEndpoint = "/notification"
-)
 
-var (
-	healthCheckExpiry time.Time
-	lastNotification  time.Time
-)
-
-func main() {
-	runtime.GOMAXPROCS(0)
-
-	port := flag.String("port", ":9094", "Port to serve at (e.g. :9094).")
-	pingTarget := flag.String("ping-target", "1.1.1.1", "Host to periodically ping (e.g. 1.1.1.1).")
-	healthcheck := flag.String("healthcheck", "", "Healthcheck service to ping every 5 minutes (currently supported: healthchecks.io:<check-id>).")
-	grafanaURL := flag.String("grafana-url", os.Getenv("GRAFANA_URL"), "Grafana host (e.g.: https://grafana.example.com).")
-	grafanaAuthToken := flag.String("grafana-auth-token", os.Getenv("GRAFANA_AUTH_TOKEN"), "Grafana authorization token.")
-	logFile := flag.String("log", "", "Log file path (defaults to empty, i.e. STDOUT).")
-	flag.Parse()
-
-	healthCheckExpiry = time.Now()
-
-	var logWriter io.Writer = os.Stderr
-	if *logFile != "" {
-		lf, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-		if err != nil {
-			log.Fatalf("Error creating log file: %v\n", err)
-		}
-		defer lf.Close()
-
-		logWriter = lf
-	}
-	logger := log.New(logWriter, "", log.LstdFlags)
-
-	// Setup our Ctrl+C handler
-	exitCh := make(chan os.Signal, 1)
-	signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
-
-	e := prometheus.NewExporter(*pingTarget, logger)
-
-	err := serveHTTP(e, *port, *grafanaURL, *grafanaAuthToken, *healthcheck, logger, exitCh)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	os.Exit(1)
-}
-
-func handleRootHTTPRequest(w http.ResponseWriter, r *http.Request, e exporter.Exporter, grafanaURL string, logger *log.Logger) {
-	w.Header().Add("Content-Type", "text/html")
-
-	s := e.Status()
-
-	lf := ""
-	if !s.LastFetch.IsZero() {
-		lf = s.LastFetch.Format(time.RFC850)
-	}
-	_, _ = w.Write([]byte(fmt.Sprintf(`
+	rootHeadHtml = `
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<style>
@@ -86,6 +33,8 @@ func handleRootHTTPRequest(w http.ResponseWriter, r *http.Request, e exporter.Ex
 	</head>
 
 	<body>
+`
+	rootMetricsHtmlFragment = `
 		<p>
 			<a href="%s">%s</a>
 			<table style="margin-left: 2em; margin-top: 1em">
@@ -125,7 +74,86 @@ func handleRootHTTPRequest(w http.ResponseWriter, r *http.Request, e exporter.Ex
 				</tbody>
 			</table>
 		</p>
-	`,
+	`
+	rootNotificationsHtmlFragment = `
+	<p>
+		<div>%s</div>
+		<table style="margin-left: 2em; margin-top: 1em">
+			<tr>
+				<th>Property</th>
+				<th>Value</th>
+			</tr>
+			<tbody>
+				<tr>
+					<th>Last notification</th>
+					<td>%s</td>
+				</tr>
+			</tbody>
+		</table>
+	</p>
+`
+)
+
+var (
+	healthCheckExpiry time.Time
+	lastNotification  time.Time
+)
+
+type httpEndpointArgs struct {
+	exporter                     exporter.Exporter
+	grafanaURL, grafanaAuthToken string
+	healthcheck                  string
+	logger                       *log.Logger
+}
+
+func main() {
+	runtime.GOMAXPROCS(0)
+
+	port := flag.String("port", ":9094", "Port to serve at (e.g. :9094).")
+	pingTarget := flag.String("ping-target", "1.1.1.1", "Host to periodically ping (e.g. 1.1.1.1).")
+	healthcheck := flag.String("healthcheck", "", "Healthcheck service to ping every 5 minutes (currently supported: healthchecks.io:<check-id>).")
+	grafanaURL := flag.String("grafana-url", os.Getenv("GRAFANA_URL"), "Grafana host (e.g.: https://grafana.example.com).")
+	grafanaAuthToken := flag.String("grafana-auth-token", os.Getenv("GRAFANA_AUTH_TOKEN"), "Grafana authorization token.")
+	logFile := flag.String("log", "", "Log file path (defaults to empty, i.e. STDOUT).")
+	flag.Parse()
+
+	healthCheckExpiry = time.Now()
+
+	var logWriter io.Writer = os.Stderr
+	if *logFile != "" {
+		lf, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatalf("Error creating log file: %v\n", err)
+		}
+		defer lf.Close()
+
+		logWriter = lf
+	}
+	logger := log.New(logWriter, "", log.LstdFlags)
+
+	// Setup our Ctrl+C handler
+	exitCh := make(chan os.Signal, 1)
+	signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+	e := prometheus.NewExporter(*pingTarget, logger)
+
+	err := serveHTTP(e, *port, *grafanaURL, *grafanaAuthToken, *healthcheck, logger, exitCh)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	os.Exit(1)
+}
+
+func handleRootHTTPRequest(w http.ResponseWriter, r *http.Request, args httpEndpointArgs) {
+	w.Header().Add("Content-Type", "text/html")
+
+	s := args.exporter.Status()
+
+	lf := ""
+	if !s.LastFetch.IsZero() {
+		lf = s.LastFetch.Format(time.RFC850)
+	}
+	_, _ = w.Write([]byte(fmt.Sprintf(rootHeadHtml+rootMetricsHtmlFragment,
 		metricsEndpoint, metricsEndpoint,
 		lf,
 		s.LastFetchDuration,
@@ -134,82 +162,72 @@ func handleRootHTTPRequest(w http.ResponseWriter, r *http.Request, e exporter.Ex
 		strings.Join(s.Devices, ", "),
 		strings.Join(s.Volumes, ", "),
 		strings.Join(s.Interfaces, ", "))))
-	if grafanaURL != "" {
+	if args.grafanaURL != "" {
 		ln := ""
 		if !lastNotification.IsZero() {
 			ln = lastNotification.Format(time.RFC850)
 		}
-		_, _ = w.Write([]byte(fmt.Sprintf(`
-		<p>
-			<div>%s</div>
-			<table style="margin-left: 2em; margin-top: 1em">
-				<tr>
-					<th>Property</th>
-					<th>Value</th>
-				</tr>
-				<tbody>
-					<tr>
-						<th>Last notification</th>
-						<td>%s</td>
-					</tr>
-				</tbody>
-			</table>
-		</p>
-	`,
-			notificationEndpoint, ln)))
+		_, _ = w.Write([]byte(fmt.Sprintf(rootNotificationsHtmlFragment, notificationEndpoint, ln)))
 	}
 	_, _ = w.Write([]byte(`
 	<body>
 	`))
 }
 
-func handleMetricsHTTPRequest(w http.ResponseWriter, r *http.Request, e exporter.Exporter, healthcheck string, logger *log.Logger) {
+func handleMetricsHTTPRequest(w http.ResponseWriter, r *http.Request, args httpEndpointArgs) {
 	w.Header().Add("Content-Type", "text/plain")
 
-	handleHealthcheckStart(healthcheck)
+	handleHealthcheckStart(args.healthcheck)
 
-	err := e.WriteMetrics(w)
+	err := args.exporter.WriteMetrics(w)
 	if err != nil {
-		logger.Println(err.Error())
+		args.logger.Println(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-	handleHealthcheckEnd(healthcheck, err)
+	handleHealthcheckEnd(args.healthcheck, err)
 }
 
-func handleNotificationHTTPRequest(r *http.Request, grafanaURL, grafanaAuthToken string, logger *log.Logger) {
+func handleNotificationHTTPRequest(r *http.Request, args httpEndpointArgs) {
 	text := r.URL.Query().Get("text")
-	url := fmt.Sprintf("%s/api/annotations", grafanaURL)
+	url := fmt.Sprintf("%s/api/annotations", args.grafanaURL)
 	body := strings.NewReader(fmt.Sprintf(`{"tags":["nas"],"text":%q}`, text))
 
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
-		logger.Printf("Error creating Grafana annotation request: %v\n", err)
+		args.logger.Printf("Error creating Grafana annotation request: %v\n", err)
 		return
 	}
 	req.Header.Set("Content-Type", "application/json")
-	if grafanaAuthToken != "" {
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", grafanaAuthToken))
+	if args.grafanaAuthToken != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", args.grafanaAuthToken))
 	}
 
 	c := http.Client{Timeout: 5 * time.Second}
 	resp, err := c.Do(req)
 	if err == nil {
-		logger.Printf("Created Grafana annotation at %s: %s\n", url, resp.Status)
+		args.logger.Printf("Created Grafana annotation at %s: %s\n", url, resp.Status)
 	} else {
-		logger.Printf("Error creating Grafana annotation at %s: %v\n", url, err)
+		args.logger.Printf("Error creating Grafana annotation at %s: %v\n", url, err)
 	}
 }
 
 func serveHTTP(e exporter.Exporter, port string, grafanaURL, grafanaAuthToken, healthcheck string, logger *log.Logger, exitCh chan os.Signal) error {
 	defer e.Close()
 
+	args := httpEndpointArgs{
+		exporter:         e,
+		healthcheck:      healthcheck,
+		grafanaURL:       grafanaURL,
+		grafanaAuthToken: grafanaAuthToken,
+		logger:           logger,
+	}
 	// handle route using handler function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { handleRootHTTPRequest(w, r, e, grafanaURL, logger) })
-	http.HandleFunc(metricsEndpoint, func(w http.ResponseWriter, r *http.Request) { handleMetricsHTTPRequest(w, r, e, healthcheck, logger) })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { handleRootHTTPRequest(w, r, args) })
+	http.HandleFunc(metricsEndpoint, func(w http.ResponseWriter, r *http.Request) { handleMetricsHTTPRequest(w, r, args) })
 	if grafanaURL != "" {
 		http.HandleFunc(notificationEndpoint, func(_ http.ResponseWriter, r *http.Request) {
-			handleNotificationHTTPRequest(r, grafanaURL, grafanaAuthToken, logger)
+			handleNotificationHTTPRequest(r, args)
 		})
 	}
 
